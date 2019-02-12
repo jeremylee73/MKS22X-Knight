a0268908def9c4fa7f38046cecfcd1dac2c3b0ba
Jeremy Lee <jlee00@stuy.edu>
2019-02-12 14:30:06 -0500

    Added exceptions to inputs

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,11 +1,15 @@
 public class KnightBoard{
   int[][] board;
+  int maxRows;
+  int maxCols;
 
   public KnightBoard(int startingRows,int startingCols){
     if (startingRows < 0 || startingCols < 0){
       throw new IllegalArgumentException();
     }
     board = new int[startingRows][startingCols];
+    maxRows = startingRows;
+    maxCols = startingCols;
   }
 
   public String toString(){
@@ -13,11 +17,30 @@ public class KnightBoard{
   }
 
   public boolean solve(int startingRow, int startingCol){
-
+    if (startingRow < 0 || startingCol < 0 || startingRow > maxRows || startingCol > maxCols){
+      throw new IllegalArgumentException();
+    }
+    for (int row=0; row<maxRows; row++){
+      for (int col=0; col<maxCols; col++){
+        if (board[row][col] != 0){
+          throw new IllegalStateException();
+        }
+      }
+    }
     return true;
   }
 
   public int countSolutions(int startingRow, int startingCol){
+    if (startingRow < 0 || startingCol < 0 || startingRow > maxRows || startingCol > maxCols){
+      throw new IllegalArgumentException();
+    }
+    for (int row=0; row<maxRows; row++){
+      for (int col=0; col<maxCols; col++){
+        if (board[row][col] != 0){
+          throw new IllegalStateException();
+        }
+      }
+    }
     return 0;
   }
 
