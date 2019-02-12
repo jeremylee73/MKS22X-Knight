c18e0f36f3a8fcef20a581a0738f8e37ba69c8a3
Jeremy Lee <jlee00@stuy.edu>
2019-02-12 14:25:17 -0500

    Added constructor

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,6 +1,11 @@
 public class KnightBoard{
-  public KnightBoard(int startingRows,int startingCols){
+  int[][] board;
 
+  public KnightBoard(int startingRows,int startingCols){
+    if (startingRows < 0 || startingCols < 0){
+      throw new IllegalArgumentException();
+    }
+    board = new int[startingRows][startingCols];
   }
 
   public String toString(){
@@ -8,6 +13,7 @@ public class KnightBoard{
   }
 
   public boolean solve(int startingRow, int startingCol){
+
     return true;
   }
 
