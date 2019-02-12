5b3ad4e32801fb66f49a09eadd27ec541b3c982c
Jeremy Lee <jlee00@stuy.edu>
2019-02-12 14:22:35 -0500

    Added template for methods

---
+++ KnightBoard.java
@@ -0,0 +1,22 @@
+public class KnightBoard{
+  public KnightBoard(int startingRows,int startingCols){
+
+  }
+
+  public String toString(){
+    return "";
+  }
+
+  public boolean solve(int startingRow, int startingCol){
+    return true;
+  }
+
+  public int countSolutions(int startingRow, int startingCol){
+    return 0;
+  }
+
+  private boolean solveH(int row ,int col, int level){
+    return true;
+  }
+  // level is the # of the knight
+}
